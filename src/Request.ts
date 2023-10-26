30e9700ca9dc05362dc14bdde25dc72e7c3ecf58
RakeshRoy1995 <rakeshroyshuvo@gmail.com>
2023-10-26 16:55:38 +0600

    request

--- src/Request.ts
+++ src/Request.ts
@@ -1,3 +1,17 @@
 import axios from "axios";
 
 const API_URL = import.meta.env.VITE_REACT_APP_API_URL;
+
+
+export function test(data:any) {
+    const page_list = `${API_URL}/user/customerLogin`;
+  
+    const options = {
+      method: "POST",
+      headers: { "content-type": "application/x-www-form-urlencoded" },
+      data,
+      url: page_list,
+    };
+  
+    return axios(options);
+  }
