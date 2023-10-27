6fea3493bc52b6164129ed42491ccc1edd5024ba
RakeshRoy1995 <rakeshroyshuvo@gmail.com>
2023-10-27 18:56:06 +0600

    ddd

--- src/component/PrivateRoute.tsx
+++ src/component/PrivateRoute.tsx
@@ -2,8 +2,8 @@ import { Navigate, Outlet } from 'react-router-dom';
 
 const PrivateRoute = ({ children, ...rest } :any) => {
     const auth :any = JSON.parse(localStorage.getItem("customer_login_auth"))
-    return auth ? <Outlet /> : <Navigate to="/login" />;
-    // return auth ? <Navigate to="/login" />   : <Outlet />;
+    // return auth ? <Outlet /> : <Navigate to="/login" />;
+    return auth ? <Navigate to="/login" />   : <Outlet />;
 
 };
 
