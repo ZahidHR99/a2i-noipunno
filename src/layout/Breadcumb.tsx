cffe708c295a034f8971c7b8f56204b01f57c456
Neloy2024 <mehdihasanneloy@Hotmail.com>
2023-11-02 10:07:01 +0600

    minor changes in multiple components

--- src/layout/Breadcumb.tsx
+++ src/layout/Breadcumb.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-    import { FiStar } from "react-icons/fi";
+
     import { AiOutlineHome } from "react-icons/ai";
     import { BiSidebar } from "react-icons/bi";
     import { MdArrowBackIosNew, MdOutlineArrowForwardIos } from "react-icons/md";
     import styles from "../component/Home.style.module.css";
-    import { HiOutlineDotsVertical } from "react-icons/hi";
+
 import { useNavigate } from 'react-router-dom';
 
 
@@ -15,52 +15,57 @@ export default function Breadcumbtitle({title} :any) {
     const navigate = useNavigate()
   return (
     <div>
+        
+        <section className="np-breadcumb-section pt-3">
    
-       
-          <div className="d-flex align-items-center py-2 gap-2 ">
-                    <div className="card shadow-lg border-0 p-2">
-                    <button
+        <div className="d-flex align-items-center py-2 gap-2">
+                <div >
+                <button className="card shadow-lg border-0 p-2"
       onClick={() => {
         navigate(-1)
       }}
     >
       <MdArrowBackIosNew className="fs-1" />
     </button>
-                      
-                    </div>
-                    <div className="card shadow-lg border-0 p-1 w-100">
-                      <div className="d-flex justify-content-between">
-                        <div>
-                          <div style={{ fontSize: "14px" }}>
-                            <BiSidebar
-                              className={`fs-3 ${styles.teacher_info_list}`}
-                            />{" "}
-                            {title}
-                          </div>
-                          <div style={{ marginLeft: "2rem" }}>
-                            <h6 style={{ color: "#C8DFDF", fontSize: "10px" }}>
-                              {" "}
-                              <AiOutlineHome />
-                              Dashboard{" "}
-                              <span style={{ color: "#000" }}>
-                                {" "}
-                                <MdOutlineArrowForwardIos />
-                                Data
-                              </span>
-                            </h6>
-                          </div>
-                        </div>
-                        {/* <div className="d-flex gap-2 align-items-center fs-4">
-                          <FiStar /> <HiOutlineDotsVertical />
-                        </div> */}
+                </div>
+                <div className="card shadow-lg border-0 p-1 w-100">
+                  <div className="d-flex justify-content-between">
+                    <div>
+                      <div style={{ fontSize: "14px" }}>
+                        <BiSidebar
+                          className={`fs-3 ${styles.teacher_info_list}`}
+                        />{" "}
+                        মূল্যায়ন প্রতিবেদন
+                      </div>
+                      <div style={{ marginLeft: "2rem" }}>
+                        <h6 style={{ color: "#C8DFDF", fontSize: "10px" }}>
+                          {" "}
+                          <AiOutlineHome />
+                          Dashboard{" "}
+                          <span style={{ color: "#000" }}>
+                            {" "}
+                            <MdOutlineArrowForwardIos />
+                            Data
+                          </span>
+                        </h6>
                       </div>
                     </div>
+                    {/* <div className="d-flex gap-2 align-items-center fs-4">
+                      <FiStar /> <HiOutlineDotsVertical />
+                    </div> */}
                   </div>
+                </div>
+              </div>
+          
       
     
     
     
     
+    </section>
     </div>
   )
 }
+
+
+
