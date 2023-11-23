546ee27aac2a7749cc0234aaba455be2fdb39120
arafat <shakhawat.softdev@gmail.com>
2023-11-23 12:57:17 +0600

    yyy

--- src/component/StudentList.tsx
+++ src/component/StudentList.tsx
@@ -76,8 +76,6 @@ const StudentList = () => {
   console.log("student", student);
 
   return (
-
-
     <>
       <Breadcumb title={"শিক্ষার্থীর তালিকা"} />
       {(student?.length == 0) ? <div className={styles.loading_container}><Spinner animation="border" /> </div> : <div className="container" my-5>
