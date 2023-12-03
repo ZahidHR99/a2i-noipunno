a70a7baa1d1c17d52cfa6952635433288c0776be
arafat <shakhawat.softdev@gmail.com>
2023-12-03 18:29:09 +0600

    Total Info interegrated real time

--- src/component/Dashboards/TotalNumberOfSudentTeacherClassRoom.tsx
+++ src/component/Dashboards/TotalNumberOfSudentTeacherClassRoom.tsx
@@ -4,6 +4,7 @@ import "./TotalNumberOfStudent.css"
 
 const TotalNumberOfSudentTeacherClassRoom = () => {
   const [all_student, set_All_student] = useState([]);
+  const [loading, setLoadin] = useState(true);
   const student_lsit = async () => {
     const student: any = [];
     const studentsData = JSON.parse(localStorage.getItem("own_subjet"));
@@ -35,14 +36,37 @@ const TotalNumberOfSudentTeacherClassRoom = () => {
     const local_storege_data = JSON.parse(
       localStorage.getItem("teacher_dashboard")
     );
-    set_Total_class(local_storege_data?.data?.classes);
+
+    if (local_storege_data?.data?.classes) {
+      set_Total_class(local_storege_data?.data?.classes);
+      setLoadin(false)
+    }
+
+
   };
 
-  useEffect(() => {
-    student_lsit();
-    teacher_list();
-    all_class();
-  }, []);
+
+  setTimeout(() => {
+
+    if (loading) {
+
+      // console.log(777777777);
+
+      student_lsit();
+      teacher_list();
+      all_class();
+
+    }
+
+
+  }, 1000)
+
+
+  // useEffect(() => {
+  //   student_lsit();
+  //   teacher_list();
+  //   all_class();
+  // }, [loading]);
 
   return (
     <div className="col-lg-2 col-md-6 ">
