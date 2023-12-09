9d1bfad7b13b91e66a3aa641d69bdae54ffab7ff
RakeshRoy1995 <rakeshroyshuvo@gmail.com>
2023-12-09 20:46:59 +0600

    navbar footer

--- src/App.tsx
+++ src/App.tsx
@@ -41,7 +41,7 @@ function App() {
 
         // const data_dash: any = await teacher_dashboard();
         // localStorage.setItem("teacher_dashboard", JSON.stringify(data_dash.data));
-        setRender(false)
+        // setRender(false)
       }
     }
 
@@ -82,14 +82,14 @@ function App() {
           <Route path="/head-teacher-dashboard" element={<HeadTeacherDashboard />} />
           <Route path="/faq" element={<FAQ />} />
         </Route>
-        {/* <Route path="/login" element={<Login />} /> */}
+
         <Route path="/login" element={<LoginPage />} />
         <Route path="/password/reset" element={<PasswordReset />} />
-
 
       </Routes>
 
       {topbar && <Footer />}
+
     </>
   );
 }
--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,35 +1,53 @@
+import { useState } from "react";
+
 export default function Footer() {
+  const [userDetails, setuserDetails] = useState<any>("");
+  
+
+
+  setTimeout(() => {
+    
+    if (!userDetails?.email) {
+      const items = JSON.parse(localStorage.getItem("customer_login_auth"));
+      if (items) {
+        setuserDetails(items.user);
+      }
+    }
+  }, 500);
+
   return (
     <div className="container-fluid  fixed-bottom  mt-4">
-      <div className="container noipunno-footer d-flex justify-content-between ">
-        <div style={{ fontWeight: "bold", fontSize: 12 }}>
-          সর্বস্বত্ব সংরক্ষিত © 2023 শিক্ষা মন্ত্রণালয়, গণপ্রজাতন্ত্রী বাংলাদেশ
-          সরকার
+      {userDetails?.email && (
+        <div className="container noipunno-footer d-flex justify-content-between ">
+          <div style={{ fontWeight: "bold", fontSize: 12 }}>
+            সর্বস্বত্ব সংরক্ষিত © 2023 শিক্ষা মন্ত্রণালয়, গণপ্রজাতন্ত্রী
+            বাংলাদেশ সরকার
+          </div>
+          <div className="d-flex">
+            <a
+              href="#"
+              className="ms-2 text-decoration-none text-secondary"
+              style={{ fontSize: 12 }}
+            >
+              কপিরাইট
+            </a>
+            <a
+              href="#"
+              className="ms-2 text-decoration-none text-secondary"
+              style={{ fontSize: 12 }}
+            >
+              গোপনীয়তা নীতি
+            </a>
+            <a
+              href="#"
+              className="ms-2 text-decoration-none text-secondary"
+              style={{ fontSize: 12 }}
+            >
+              জিজ্ঞাসা
+            </a>
+          </div>
         </div>
-        <div className="d-flex">
-          <a
-            href="#"
-            className="ms-2 text-decoration-none text-secondary"
-            style={{ fontSize: 12 }}
-          >
-            কপিরাইট
-          </a>
-          <a
-            href="#"
-            className="ms-2 text-decoration-none text-secondary"
-            style={{ fontSize: 12 }}
-          >
-            গোপনীয়তা নীতি
-          </a>
-          <a
-            href="#"
-            className="ms-2 text-decoration-none text-secondary"
-            style={{ fontSize: 12 }}
-          >
-            জিজ্ঞাসা
-          </a>
-        </div>
-      </div>
+      )}
     </div>
   );
 }
